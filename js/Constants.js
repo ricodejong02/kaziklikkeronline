e641fdba830f1f7824a1eca751d8ac0f53e5edfe
Daniel Verhoef <danielverhoef@hotmail.nl>
2016-12-14 17:16:39 +0100

    Revert "fixed"
    This reverts commit 4cd4738bb9690d158b5e6b5c803a1af4a99cee34.

--- js/Constants.js
+++ js/Constants.js
@@ -2,13 +2,13 @@
  * Created by Daniel Verhoef on 13-12-2016.
  */
 var DataBC = {
-    clicker: {name: "Klikker", price: 250, speed: 1, increase: 25, amount: 0},
-    farm: {name: "Boerderij", price: 2000, speed: 5, increase: 200, amount: 0},
-    mine: {name: "Mijn", price: 50000, speed: 50, increase: 5000, amount: 0},
-    village: {name: "Dorp", price: 650000, speed: 100, increase: 65000, amount: 0},
-    city: {name: "Stad", price: 1000000, speed: 1000, increase: 100000, amount: 0},
-    country: {name: "Land", price: 45000000, speed: 5000, increase: 4500000, amount: 0},
-    planet: {name: "Planeet", price: 100000000, speed: 10000, increase: 10000000, amount: 0},
-    galaxy: {name: "Melkweg", price: 7500000000, speed: 50000, increase: 750000000, amount: 0},
-    universe: {name: "Universum", price: 10000000000, speed: 500000, increase: 1000000000, amount: 0}
+    clicker: {name: "Klikker", price: 25, speed: 1, increase: 4, amount: 0},
+    farm: {name: "Boerderij", price: 2000, speed: 5, increase: 8, amount: 0},
+    mine: {name: "Mijn", price: 50000, speed: 50, increase: 11, amount: 0},
+    village: {name: "Dorp", price: 650000, speed: 100, increase: 19, amount: 0},
+    city: {name: "Stad", price: 1000000, speed: 1000, increase: 27, amount: 0},
+    country: {name: "Land", price: 45000000, speed: 5000, increase: 35, amount: 0},
+    planet: {name: "Planeet", price: 100000000, speed: 10000, increase: 79, amount: 0},
+    galaxy: {name: "Melkweg", price: 7500000000, speed: 50000, increase: 122, amount: 0},
+    universe: {name: "Universum", price: 10000000000, speed: 500000, increase: 3535, amount: 0}
 };
